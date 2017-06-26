4b633f615a23be40e591719d4038096480409084
Jonas Eliasson <jonas.eliasson@comhem.com>
2017-06-26 15:46:44 +0200

    Add kubernetes install and configure script

--- install_setup_kubernetes.sh
+++ install_setup_kubernetes.sh
@@ -8,6 +8,24 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 ### Create config
 mkdir ${HOME}/.kube
 cat > ${HOME}/.kube/config << EOF
-Hello on ${SOME_SECRET}!
+---
+apiVersion: v1
+clusters:
+  - cluster:
+      certificate-authority-data: ${CERTIFICATE_AUTHORITY_DATA}
+      server: ${CLUSTER_SERVER}
+    name: "${CLUSTER_NAME}"
+contexts:
+  - context:
+      cluster: "${CLUSTER_NAME}"
+      user: "${CLUSTER_NAME}-admin"
+    name: "${CLUSTER_NAME}"
+current-context: "${CLUSTER_NAME}"
+kind: Config
+users:
+  - name: "${CLUSTER_NAME}-admin"
+    user:
+      client-certificate-data: ${CLIENT_CERTIFICATE_DATA}
+      client-key-data: ${CLIENT_KEY_DATA}
 EOF
 
