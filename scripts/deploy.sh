25a89b78ed04210b95c8b623dfbe271d66408f03
Jonas Eliasson <jonas.eliasson@comhem.com>
2017-06-27 17:12:03 +0200

    Update .travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,4 +21,5 @@ docker_registry=`printenv DOCKER_REGISTRY_${env}`
 
 docker login -u=${docker_username} -p=${docker_password} ${docker_registry}
 docker build -t "${docker_registry}/quantum" .
+kubectl get nodes
 echo "deploying ${BRANCH} (Env -> ${env})"
